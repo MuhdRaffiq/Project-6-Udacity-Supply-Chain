9c427b05f7859234bba0bd1201b84d9a40fb7f03
Muhammad Raffiq Mohd Khalil <muhammadraffiq@gmail.com>
2020-12-13 23:53:33 +0800

    First commit

---
+++ truffle-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }, 
+    /*mynetwork: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // match any network
+    }, */
+  }
+};
